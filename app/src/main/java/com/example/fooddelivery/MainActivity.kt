7fb7099de66b0f47138ea4f3609b40078d83ffb6
ri_yarm <thogusus@gmail.com>
2024-01-08 00:19:16 +0500

    add: bottom navigation

--- app/src/main/java/com/example/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/example/fooddelivery/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.fooddelivery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.fooddelivery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        changeFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_home -> {
+                    changeFragment(HomeFragment())
+                }
+
+                R.id.menu_cart -> {
+                    changeFragment(CartFragment())
+                }
+
+                R.id.menu_search -> {
+                    changeFragment(SearchFragment())
+                }
+
+                R.id.menu_history -> {
+                    changeFragment(HistoryFragment())
+                }
+
+                R.id.menu_profile -> {
+                    changeFragment(ProfileFragment())
+                }
+            }
+
+            return@setOnItemSelectedListener true
+        }
+    }
+
+    private fun changeFragment(activity: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.fragment_container, activity)
+        fragmentTransaction.commit()
     }
 }
